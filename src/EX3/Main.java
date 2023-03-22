7f1832e4c29e1668db7f5f0fe86d14fa3bec6fdb
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-22 13:41:47 +0100

    scrivi file ex3

--- src/EX3/Main.java
+++ src/EX3/Main.java
@@ -1,7 +1,55 @@
 package EX3;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
+
+    private static String handle = "";
+    public static Map<String, Integer> elenco = new HashMap<String, Integer>();
+    private static File f = new File("w2l3ex3.txt");
+
     public static void main(String[] args) {
-        
+
+        if (leggiFile(f) != "") {
+            System.out.println(leggiFile(f));
+        } else {
+            System.out.println("Il file è vuoto!");
+        }
+
+        elenco.put("Mario Rossi", 5);
+        elenco.put("Giorgio Bianchi", 7);
+        elenco.put("Gianni Verdi", 7);
+
+        Set<String> keys = elenco.keySet();
+
+        for (String k : keys) {
+            handle += k + "@" + elenco.get(k) + "#";
+        }
+
+        scriviFile(f, handle);
+
+    }
+
+    public static void scriviFile(File f, String s) {
+        try {
+            FileUtils.writeStringToFile(f, s, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String leggiFile(File f) {
+        try {
+            return ((FileUtils.readFileToString(f, "UTF-8")).replace("@", ",")).replace("#", ",");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
